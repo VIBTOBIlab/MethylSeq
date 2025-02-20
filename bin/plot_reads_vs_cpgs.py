f6137e9f1fbda299435a8a3d2f098499e25eb10b
edogiuili <edoardogiuili@gmail.com>
2025-02-20 09:06:46 +0100

    added sq sat values

--- bin/plot_reads_vs_cpgs.py
+++ bin/plot_reads_vs_cpgs.py
@@ -29,7 +29,7 @@ def plot_data(x_data,y_data,reads,asymptote,params,output_plot,title):
     xpred = np.append(x_data, np.array( [1.2,1.4,1.6,1.8,2] ) )
     xpred_reads = np.array( reads*xpred )
     ypred = asymptotic_growth(xpred, *params)
-    y_diff = [0,1]+[(ypred[i]-ypred[i-1])/ypred[i-1]*100 for i in range(2,len(ypred))]
+    y_diff = [0,1]+[(ypred[i]/asymptote)*100 for i in range(2,len(ypred))]
     
     # Create the figure and axis
     fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -44,14 +44,13 @@ def plot_data(x_data,y_data,reads,asymptote,params,output_plot,title):
 
     # Add text labels for y_diff below the plot
     for i in range(4, len(xpred)):
-        ax1.text(xpred[i], ypred[i] - 0.07 * ypred[i], f"{y_diff[i]:.2f}%", color="black", fontsize=10, ha="center")
+        ax1.text(xpred[i], ypred[i] - 0.09 * ypred[i], f"{y_diff[i]:.1f}%", color="black", fontsize=10, ha="center")
 
     # Add asymptote line
     ax1.axhline(y=asymptote, color='grey', linestyle='--')
-    ax1.axhline(y=y_data[-1], color='r', linestyle='--')
 
     # Set title and labels for the bottom x-axis
-    ax1.set_title(title, fontsize=16)
+    ax1.set_title(title, fontsize=16, loc= "left")
     ax1.set_xlabel("Percentage of downsampling", fontsize=14)
     ax1.set_ylabel("Number of CpGs", fontsize=14)
 
@@ -61,13 +60,12 @@ def plot_data(x_data,y_data,reads,asymptote,params,output_plot,title):
     ax2.set_xticks(xpred)
     ax2.tick_params(axis='x',pad=7)
     ax2.set_xticklabels([f'{x:.1e}' for x in xpred_reads], fontsize=10, rotation=45)
-
+    ax2.set_xlabel("Number of reads", fontsize=14)
     # Customize the x-tick positions and move them to the top
     ax2.xaxis.set_ticks_position('top')
     ax1.xaxis.set_ticks_position('bottom')
-    leg_patch = mpatches.Patch(label = r"   % : CpGs percentage growth ($\frac{y_i - y_{i-1}}{y_{i-1}} \times 100$)")
-    ax1.text(1.35, asymptote*0.13, f"Asymptote = {asymptote:.2e}", color="grey", fontsize=10)
-    ax1.text(1.35, asymptote*0.08, f"Sequencing saturation = {y_data[-1]/asymptote*100:.2f}%", color="red", fontsize=10)
+    leg_patch = mpatches.Patch(label = r"   % : Sequencing saturation ($\frac{\hat y}{\text{asymptote}} \times 100$)")
+    ax1.text(0, asymptote-asymptote*0.05, f"Asymptote = {asymptote:.2e}", color="grey", fontsize=12)
     plt.legend(handles=[leg_patch],loc="lower right",handletextpad=-1.0, handlelength=0)
 
     # Display grid and layout
