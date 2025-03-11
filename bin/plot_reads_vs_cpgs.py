c77ba831466271c489c6718b05d0e21414db0b22
edogiuili <edoardogiuili@gmail.com>
2025-03-11 17:52:40 +0100

    fixed error exit in seq sat

--- bin/plot_reads_vs_cpgs.py
+++ bin/plot_reads_vs_cpgs.py
@@ -7,6 +7,8 @@ import matplotlib.patches as mpatches
 import argparse
 from scipy.optimize import curve_fit
 from scipy.optimize import fsolve
+import warnings
+from scipy.optimize import OptimizeWarning
 
 
 def asymptotic_growth(x, beta0, beta1):
@@ -22,6 +24,44 @@ def find_asymptote(params):
     asymptote = beta0 * np.pi / 2 # Limit for x that tends to Inf
     return asymptote
 
+def plot_error_data(x_data,y_data,reads,output_plot,title,error):
+    # Predict new data
+    xpred = np.append(x_data, np.array( [1.2,1.4,1.6,1.8,2] ) )
+    xpred_reads = np.array( reads*xpred )
+    
+    # Create the figure and axis
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+
+    # Plot the main data (on bottom x-axis)
+    ax1.plot(x_data, y_data, marker="o", color="blue", linestyle="")
+
+    # Plot the fit line
+    ax1.set_xticks(xpred)
+
+    # Set title and labels for the bottom x-axis
+    ax1.set_title(title + "\n" + error, fontsize=12, color = "red", loc= "left")
+    ax1.set_xlabel("Percentage of downsampling", fontsize=14)
+    ax1.set_ylabel("Number of CpGs", fontsize=14)
+
+    # Plot the second x-axis for xpred_reads (top x-axis)
+    ax2 = ax1.twiny()  # Create a second x-axis sharing the same y-axis
+    ax2.set_xlim(ax1.get_xlim())  
+    ax2.set_xticks(xpred)
+    ax2.tick_params(axis='x',pad=7)
+    ax2.set_xticklabels([f'{x:.1e}' for x in xpred_reads], fontsize=10, rotation=45)
+    ax2.set_xlabel("Number of reads", fontsize=14)
+    # Customize the x-tick positions and move them to the top
+    ax2.xaxis.set_ticks_position('top')
+    ax1.xaxis.set_ticks_position('bottom')
+
+    # Display grid and layout
+    ax1.grid(True, linestyle="--", alpha=0.6)
+    fig.tight_layout()
+
+    # Save the plot
+    plt.savefig(output_plot)
+    plt.close()
+    print(f"Plot saved to {output_plot}")
 
 def plot_data(x_data,y_data,reads,asymptote,params,output_plot,title):
 
@@ -90,17 +130,29 @@ def plot_reads_vs_cpgs(data, output_plot,percentages):
     # Add 0 to the x and y data
     x_data = np.array( [0] + percentages )
     y_data = np.array( [0] + data["cpgs_counts"].tolist() )
-
-    # Fit the function to the data
-    params, _ = curve_fit(asymptotic_growth, x_data, y_data, p0=[1, 1])
-
-    # Find the asymptote value
-    asymptote = find_asymptote(params)
+    fit_success = True
+    
+    # If doesn't find optimal params, plot just the points without the curve_fit
+    try:
+        with warnings.catch_warnings():
+            warnings.simplefilter("error", OptimizeWarning)
+            # Fit the function to the data
+            params, _ = curve_fit(asymptotic_growth, x_data, y_data, p0=[1, 1])
+        asymptote = find_asymptote(params)
+    except (RuntimeError, OptimizeWarning) as e:
+        fit_success = False
+        params = None
+        asymptote = None
+        error_msg = str(e)
+        print(f"Curve fitting error: {e}")
 
     # Plot the data and save the plots
     title = list(set(data["sample"]))[0]
     reads = int(data["reads_counts"].tolist()[-1])
-    plot_data(x_data,y_data,reads,asymptote,params,output_plot,title)
+
+    if fit_success==True:
+        plot_data(x_data,y_data,reads,asymptote,params,output_plot,title)
+    else: plot_error_data(x_data,y_data,reads,output_plot,title,error_msg)
 
 
 def select_sample(cpgs, reads,percentages):
